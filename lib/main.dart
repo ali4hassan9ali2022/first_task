2912824b9315ce44526fbdc25e86a949b394ccbe
alikasrawy <kasrawyali47@gmail.com>
2025-09-21 12:01:50 +0300

    create first step

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FirstTask());
+}
+
+class FirstTask extends StatelessWidget {
+  const FirstTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
