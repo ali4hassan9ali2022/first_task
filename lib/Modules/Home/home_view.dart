4d70a0fff3596a56851a1bb4d1247bb7d65b267c
alikasrawy <kasrawyali47@gmail.com>
2025-09-21 12:53:02 +0300

    create title for home view

--- lib/Modules/Home/home_view.dart
+++ lib/Modules/Home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:first_task/Widgets/build_app_bar.dart';
+import 'package:first_task/Widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -7,6 +8,8 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: buildAppBar(),
+
+    body: HomeViewBody(),
     );
   }
 }
---
+++ lib/Widgets/home_view_body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            "Branch",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
